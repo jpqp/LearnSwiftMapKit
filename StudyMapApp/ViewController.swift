077dcb0e1318418ee0fb508649e74010b4a432c1
yuki <affective.wep@gmail.com>
2018-06-07 00:40:10 +0900

    Map app init

--- StudyMapApp/ViewController.swift
+++ StudyMapApp/ViewController.swift
@@ -1,18 +1,13 @@
-//
-//  ViewController.swift
-//  StudyMapApp
-//
-//  Created by Yuki, Mogi on 2018/06/05.
-//  Copyright © 2018年 Yuki, Mogi. All rights reserved.
-//
-
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // ここでの挙動は、inputText.delegateに自分自身を設定する
+        inputText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +15,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var displayMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if let searchKeyword = textField.text {
+            print(searchKeyword)
+            let geocoder = CLGeocoder()
+            geocoder.geocodeAddressString(
+                searchKeyword,
+                completionHandler: {
+                    (placemarks: [CLPlacemark]?, error:Error?) in
+                    if let placemark = placemarks?[0] {
+                        if let targetCordinate = placemark.location?.coordinate {
+                            print(targetCordinate)
+                            
+                            let pin = MKPointAnnotation()
+                            pin.coordinate = targetCordinate
+                            pin.title = searchKeyword
+                            
+                            self.displayMap.addAnnotation(pin)
+                            self.displayMap.region = MKCoordinateRegionMakeWithDistance(
+                                targetCordinate,
+                                500.0,
+                                500.0
+                            )
+                        }
+                    }
+            })
+        }
+        return true
+    }
+    
 }
 
